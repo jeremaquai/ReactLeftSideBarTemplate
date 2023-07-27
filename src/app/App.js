a880475412ed0cd9af09631f40448afb5bd0a74c
jeremaquai <jeremaquai@gmail.com>
2023-07-26 21:27:36 -0500

    template finalized for now

--- src/app/App.js
+++ src/app/App.js
@@ -4,6 +4,7 @@ import Header from '../components/AppHeader/Header';
 import { BrowserRouter as Router } from 'react-router-dom/cjs/react-router-dom.min';
 import Footer from '../components/AppFooter/Footer';
 import SideBar from '../components/SideBar/SideBar';
+import PageBody from '../components/PageBody/PageBody';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <div className='SideBarAndPageBody'>
         <SideBar />
+        <PageBody />
       </div>
       <Footer />
     </Router>
